6b24adbbb1c3f079dd0575171bdf90f28b6585a3
wangkai <wangkai001_tg@163.com>
2016-04-14 17:14:56 +0800

    Add Moving function(1st)

--- Rpi_CAR.py
+++ Rpi_CAR.py
@@ -1,53 +1,57 @@
 import RPi.GPIO as io
 import time
-io.setmode(io.BOARD)
+import config as con
 
-class Wheel(object):
-    def __init__(self,in_pin1,in_pin2):
-        self.pin1 = in_pin1
-	self.pin2 = in_pin2
-	# setup I/O OUT
-	io.setup(in_pin1,io.OUT)
-	io.setup(in_pin2,io.OUT)
-    # MOVE
-    def forward(self):
-	io.output(self.pin1,True)
-	io.output(self.pin2,False)
-    def backward(self):
-	io.output(self.pin1,False)
-	io.output(self.pin2,True)
-    def stop(self):
-	io.output(self.pin1,False)
-	io.output(self.pin2,False)
-
-class Car(object):
-    def __int__(self):
-	
-	self.left_wheel = Wheel(37,36)
-	self.right_wheel = Wheel(35,38)
+class Car:
+    
+    io.setmode(io.BOARD)	
+    left_wheel =(35,38)
+    right_wheel =(37,36)
+        
+        
     #MOVE
-    def forward(self):
-	self.left_wheel.forward()
-	self.right_wheel.forward()
-    def backward(self):
-	self.left_wheel.backward()
-	self.right_wheel.backward()
     def left(self):
-	self.left_wheel.forward()
-	self.right_wheel.stop()
-    def left_point(self):
-	self.left_wheel.forward()
-	self.right_wheel.backward()
-    def right(self):
-	self.right_wheel.forward()
-	self.left_wheel.stop()
-    def right_point(self):
-	self.right_wheel.forward()
-	self.left_wheel.backward()
-    def stop(self):
-	self.left_wheel.stop()
-	self.right_wheel.stop()
-    def shutdown(self):
-	self.stop()
-	io.cleanup()
+	try:
+	    io.cleanup()
+	
+	finally:
+	    io.setup(35,io.OUT)
+	io.output(35,True)
 
+    def right(self):
+        try:
+            io.cleanup()
+
+        finally:
+            io.setup(38,io.OUT)
+        io.output(38,True)
+
+    def forward(self):
+        try:
+            io.cleanup()
+
+        finally:
+            io.setup(35,io.OUT)
+            io.setup(38,io.OUT)
+	io.output(35,True)
+	io.output(38,True)
+
+    def backward(self):
+        try:
+            io.cleanup()
+
+        finally:
+            io.setup(36,io.OUT)
+            io.setup(37,io.OUT)
+	io.output(36,True)
+	io.output(37,True)
+    def stop(self):
+        try:
+            io.cleanup()
+
+        finally:
+            print 'Stop'
+
+
+
+		
